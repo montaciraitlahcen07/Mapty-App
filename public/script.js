9fd5f809f2a92f9b29120e72fd9fb174aa0a0d69
montacir ait lahcen <montaciraitlahcen@gmail.com>
2026-05-29 23:57:09 +0100

    feat: rendering a map with the current location and having the capabilitie to add markers

--- public/script.js
+++ public/script.js
@@ -1,1 +1,55 @@
 "use strict";
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(
+    function (Position) {
+      const map = L.map("map").setView(
+        [Position.coords.latitude, Position.coords.longitude],
+        13,
+      );
+
+      L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      L.marker([Position.coords.latitude, Position.coords.longitude])
+        .addTo(map)
+        .bindPopup(
+          L.popup({
+            maxWidth: 250,
+            minWidth: 100,
+            autoClose: false,
+            closeOnClick: false,
+            className: "running-popup",
+          }),
+        )
+        .setPopupContent("Workout")
+        .openPopup();
+      map.on(
+        "click",
+        function (mapEvent) {
+          const { lat, lng } = mapEvent.latlng;
+          L.marker([lat, lng])
+            .addTo(map)
+            .bindPopup(
+              L.popup({
+                maxWidth: 250,
+                minWidth: 100,
+                autoClose: false,
+                closeOnClick: false,
+                className: "running-popup",
+              }),
+            )
+            .setPopupContent("Workout")
+            .openPopup();
+        },
+        function () {
+          console.log("couldn't get your cordinates'");
+        },
+      );
+    },
+    function () {
+      console.log("couldn't get your cordinates");
+    },
+  );
+}
