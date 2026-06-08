f09ad3c64f4428cb569f813ff16e4aa5be6e014e
montacir ait lahcen <montaciraitlahcen@gmail.com>
2026-06-08 22:02:16 +0100

    feat: workouts deletion

--- public/script.js
+++ public/script.js
@@ -13,6 +13,7 @@ class App {
   #map;
   #latlng;
   #Workouts = [];
+  #markers = new Map();
   constructor() {
     this._getPosition();
     this._getLocaleStorage();
@@ -102,17 +103,23 @@ class App {
   }
   _runningWorkout(workout) {
     const htmlWorkout = `<li
-          class="workout bg-[#42484D] rounded-[5px] px-9 py-6 mb-7 cursor-pointer grid grid-cols-[1fr_1fr_1fr_1fr] gap-x-6 gap-y-3 border-l-5 border-[#00c46a]"
+          class="workout bg-[#42484D] rounded-[5px] px-9 py-6 mb-7 cursor-pointer grid grid-cols-[1fr_1fr_1fr_1fr] gap-x-6 gap-y-3 border-l-5 border-[#00c46a] relative"
           data-id=${workout.id}
         >
+          <button
+            class="workout__delete absolute h-10 w-10 top-4 right-5 text-[#ececec] hover:text-[#fffeff] hover:bg-[#606367] text-5xl flex items-center justify-center transition-colors duration-300 cursor-pointer border-0 rounded-xl"
+          >
+            &times;
+          </button>
           <h2
-            class="text-[#ececec] workout__title text-[1.7rem] font-semibold col-span-full"
+            class="text-[#ececec] workout__title text-[1.7rem] font-semibold col-span-full pr-8"
           >
             Running on ${new Date(workout.date).toLocaleDateString("en-US", {
               month: "short",
               day: "numeric",
             })}
           </h2>
+          
           <div class="workout__details">
             <span class="workout__icon">🏃&zwj;</span>
             <span class="workout__value text-[#ececec]">${workout.distance}</span>
@@ -138,9 +145,14 @@ class App {
   }
   _cyclingWorkout(workout) {
     const htmlWorkout = `<li
-          class="workout bg-[#42484D] rounded-[5px] px-9 py-6 mb-7 cursor-pointer grid grid-cols-[1fr_1fr_1fr_1fr] gap-x-6 gap-y-3 border-l-5 border-[#FFB545]"
+          class="workout bg-[#42484D] rounded-[5px] px-9 py-6 mb-7 cursor-pointer grid grid-cols-[1fr_1fr_1fr_1fr] gap-x-6 gap-y-3 border-l-5 border-[#FFB545] relative"
           data-id=${workout.id}
         >
+        <button
+            class="workout__delete absolute h-10 w-10 top-4 right-5 text-[#ececec] hover:text-[#fffeff] hover:bg-[#606367] text-5xl flex items-center justify-center transition-colors duration-300 cursor-pointer border-0 rounded-xl"
+          >
+            &times;
+          </button>
           <h2
             class="text-[#ececec] workout__title text-[1.7rem] font-semibold col-span-full"
           >
@@ -173,7 +185,7 @@ class App {
     formElement.insertAdjacentHTML("afterend", htmlWorkout);
   }
   _renderWorkoutMarker(workout) {
-    L.marker(workout.coords)
+    const marker = L.marker(workout.coords)
       .addTo(this.#map)
       .bindPopup(
         L.popup({
@@ -204,6 +216,7 @@ class App {
         } `,
       )
       .openPopup();
+    this.#markers.set(workout.id, marker);
   }
   _renderWorkout(workout) {
     if (workout.type == "running") {
@@ -214,7 +227,8 @@ class App {
   }
   _moveToPopup(e) {
     const target = e.target.closest(".workout");
-    if (target?.closest("li")) {
+    // prettier-ignore
+    if (e.target.closest(".workout")?.tagName === "LI" && !(e.target?.tagName === "BUTTON")) {
       const elem = this.#Workouts.find(
         (workout) => workout.id == target.dataset.id,
       );
@@ -223,6 +237,7 @@ class App {
         pan: { duration: 1 },
       });
     }
+    this._workoutDeletion(e);
   }
   _setLocaleStorage() {
     localStorage.setItem("workouts", JSON.stringify(this.#Workouts));
@@ -245,6 +260,17 @@ class App {
   reset() {
     localStorage.removeItem("workouts");
     location.reload();
+  }
+  _workoutDeletion(e) {
+    const target = e.target.closest(".workout");
+    // prettier-ignore
+    if (e.target.closest(".workout")?.tagName === "LI" && (e.target?.tagName === "BUTTON")) {
+      this.#Workouts = this.#Workouts.filter((workout) => workout.id != target.dataset.id);
+      localStorage.setItem('workouts', JSON.stringify(this.#Workouts));
+      target.remove();
+      this.#markers.get(target.dataset.id).remove();
+      this.#markers.delete(target.dataset.id);
+    }
   }
 }
 class Workout {
